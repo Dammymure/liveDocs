0d7f7ad7ae3a0943dd49471be6244c5ef7fa9646
Dammymure <dammymure1@gmail.com>
2024-09-10 09:28:50 +0100

    Collaborative features

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -8,9 +8,10 @@ import { useEffect, useRef, useState } from 'react'
 import { Input } from './ui/input'
 import Image from 'next/image'
 import { updateDocument } from '@/lib/actions/room.actions'
+import Loader from './Loader'
+import ShareModal from './ShareModal'
 
-const CollaborativeRoom = ({ roomId, roomMetadata }: CollaborativeRoomProps) => {
-  const currentUserType = 'editor';
+const CollaborativeRoom = ({ roomId, roomMetadata, users, currentUserType }: CollaborativeRoomProps) => {
   const [documentTitle, setDocumentTitle] = useState(roomMetadata.title)
   const [editing, setEditing] = useState(false)
   const [loading, setLoading] = useState(false)
@@ -68,7 +69,7 @@ const CollaborativeRoom = ({ roomId, roomMetadata }: CollaborativeRoomProps) => 
 
   return (
     <RoomProvider id={roomId}>
-    <ClientSideSuspense fallback={<div>Loading…</div>}>
+    <ClientSideSuspense fallback={<Loader/>}>
       <div className='collaborative-room'>
       <Header>
             <div ref={containerRef} className='flex w-fit items-center justify-center gap-2'>
@@ -107,6 +108,12 @@ const CollaborativeRoom = ({ roomId, roomMetadata }: CollaborativeRoomProps) => 
             </div>
             <div className='flex w-full flex-1 justify-end gap-2 sm:gap-3'>
               <ActiveCollaborators/>
+              <ShareModal
+              roomId={roomId}
+              collaborators={users}
+              creatorId={roomMetadata. creatorId}
+              currentUserType={currentUserType}
+              />
 
               <SignedOut>
                 <SignInButton />
@@ -118,7 +125,7 @@ const CollaborativeRoom = ({ roomId, roomMetadata }: CollaborativeRoomProps) => 
             </div>
 
         </Header>
-        <Editor/>
+        <Editor roomId={roomId} currentUserType={currentUserType}/>
       </div>
     </ClientSideSuspense>
   </RoomProvider>
